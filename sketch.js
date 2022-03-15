bea06bdd8b20249d6d1cf06596953dd0741d4403
Arjun <73472482+Arjun-Deshpande@users.noreply.github.com>
2022-03-15 16:49:37 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var box;
+function setup() {
+  createCanvas(1440, 900);
+  console.log("Hello World!")
+  box = createSprite(100, 200, 30, 30)
+}
+
+function draw() {
+  drawSprites();
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.position.x += 5;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    box.position.x -= 5;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    box.position.y -= 5;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    box.position.y += 5;
+  }
+}
+
+
+
+
